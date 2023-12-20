b760a04d668fd38d0d7da85f97f458961e7a7778
Liv Madeleine Nitsche <liv.madeleine.nitsche@mni.thm.de>
2023-12-20 22:48:46 +0100

    Added allValidMoves() to Game.java

--- src/Model/Game.java
+++ src/Model/Game.java
@@ -19,8 +19,10 @@ public class Game implements IGame{
         Player player2 = new Player(FieldState.BLACK, 9);
 
         System.out.println(this);
-        allValidMoves(grid, 10);
 
+        for (int i = 0; i < grid.length; i++) {
+            System.out.println(allValidMoves(grid, i));
+        }
     }
 
     public boolean isEmpty(int point) {
@@ -41,7 +43,7 @@ public class Game implements IGame{
 
     public List<Integer> allValidMoves(FieldState[] grid, int from) {
         List<Integer> validMoves = new ArrayList();
-            //Points: 0,8,16
+            //Points: 0,8,16    Special
             if (from == 0 || from == 8 || from == 16) {
                 if(grid[from+1] == FieldState.FREE) {
                     validMoves.add(from+1);
@@ -50,6 +52,19 @@ public class Game implements IGame{
                     validMoves.add(from+7);
                 }
             }
+            //Points: 1,3,5,7,9,11,13, 15     +8
+            if(from == 1 || from == 3 || from == 5 || from == 7 || from == 9 || from == 11 || from ==13 || from == 15) {
+                if(grid[from +8] == FieldState.FREE) {
+                    validMoves.add(from+8);
+                }
+            }
+            //Points 9,11,13,15,17,19,21,23    -8
+            if(from == 9 || from == 11 || from ==13 || from == 15 || from == 17 || from == 19 || from == 21 || from ==23) {
+                if(grid[from-8] == FieldState.FREE) {
+                    validMoves.add(from-8);
+                }
+            }
+            //Points: 7,15,23      -7
             if(from == 7 || from == 15 || from == 23) {
                 if(grid[from-7] == FieldState.FREE) {
                     validMoves.add(from-7);
@@ -57,21 +72,10 @@ public class Game implements IGame{
                 if(grid[from-1] == FieldState.FREE) {
                     validMoves.add(from-1);
                 }
-                if(from == 7 && grid[from+8] == FieldState.FREE) {
-                    validMoves.add(from+8);
-                }
-                if((from == 15) && grid[from-8] == FieldState.FREE) {
-                    validMoves.add(from-8);
-                }
-                if((from == 15) && grid[+8] == FieldState.FREE) {
-                    validMoves.add(from+8);
-                }
-                if(from == 23 && grid[from-8] == FieldState.FREE) {
-                    validMoves.add(from-8);
-                }
             }
-            // Points: 2,4,6,10,12,14,18,20,22
-            if(from == 2 || from == 4 || from == 6 || from == 10 || from == 12 || from == 14 || from == 18 || from == 20 || from == 22) {
+            // Points: 1,2,3,4,5,6,9,10,11,12,13,14,18,20,22  +-1
+            if(from == 1|| from == 2 || from ==3 || from == 4 || from == 5 || from == 6 || from == 9 || from == 10 ||
+                    from == 11|| from == 12 || from == 13 || from == 14 ||from == 17 || from == 18 || from == 19 || from == 20 || from == 21 || from == 22) {
                 if(grid[from+1] == FieldState.FREE) {
                     validMoves.add(from+1);
                 }
@@ -79,9 +83,7 @@ public class Game implements IGame{
                     validMoves.add(from-1);
                 }
             }
-
-        System.out.println(validMoves);
-        return validMoves;
+            return validMoves;
     }
     public void updateField() {
 
